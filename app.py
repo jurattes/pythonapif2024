9a5b38757237d584e462b07ccedd7a4e597249c2
jurattes <j.pescador03@gmail.com>
2024-09-30 02:53:31 -0400

    Added app.py + Added link to MongoDB

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET'])
+def home():
+    return "HOME"
+
+client = MongoClient('mongodb+srv://jpescador03:OLI5CIAEpoQwnfPw@cluster0.gzcth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['LS3FALL2024']
+users_collection = db['Users']
+
+print(users_collection)
+
+@app.route('/users', methods = ['GET'])
+def get_users():
+    users = []
+    for user in users_collection.find():
+        user['_id'] = str(user['_id'])
+        users.append(user)
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
